8e13f2cfbf9b9e8f8e200079e15b558f13af76b0
Bardavid Nathan <nbabardavid@gmail.com>
2025-09-27 18:20:49 +0200

    lot of stuff or maybe not idk

---
+++ rofi/test.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+DIR="$HOME/Pictures"
+SYMLINK_PATH="$HOME/.config/hypr/current_wallpaper"
+
+dir="$HOME/.config/rofi/launchers/type-1"
+theme='style-10'
+
+CMD="$HOME/bin/change_wallpaper.sh"
+
+FILE=$(
+  for a in "$DIR"/*.{jpg,jpeg,png,gif}; do
+    [ -f "$a" ] || continue
+    name=$(basename "$a")
+    echo -en "$name\0icon\x1f$a\n"
+  done | rofi -dmenu -show-icons -theme "$HOME/.config/rofi/launchers/type-1/style-wallpaper" -p "Wallpaper:"
+)
+
+if [ -n "$FILE" ]; then
+    swww img "$DIR/$FILE" --transition-type center --transition-duration 2; matugen image "$DIR/$FILE"
+fi
+
+cp "$DIR/$FILE" "$SYMLINK_PATH"
